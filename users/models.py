529bc1de05781e77e5937e816c9d72a6e4c51a5f
Du <ngdu156@gmail.com>
2022-04-02 17:35:06 +0700

    Set Debug Value

--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class profile(models.Model):
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
