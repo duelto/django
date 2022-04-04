b6966011ffb2ffd199b3b8688ff7e78e7b16bdb9
Du <ngdu156@gmail.com>
2022-04-04 17:15:56 +0700

    Update Settings

--- djweb/settings.py
+++ djweb/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'blog',
     'users',
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -143,5 +144,15 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ngdu156@gmail.com'
 EMAIL_HOST_PASSWORD = 'roraovjelonbkpdn'
+
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
